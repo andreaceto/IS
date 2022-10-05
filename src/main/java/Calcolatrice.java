53ce3172517a67a6bf77560fa1015c392b7b98e2
andreaceto <aceto.andrea00@gmail.com>
2022-10-05 15:08:37 +0200

    Implementata classe Calcolatrice

---
+++ src/main/java/Calcolatrice.java
@@ -0,0 +1,25 @@
+public class Calcolatrice {
+
+    private int a, b;
+
+    public Calcolatrice() {
+        this.a = 0;
+        this.b = 0;
+    }
+    public int somma(int a, int b) {
+        return a+b;
+    }
+
+    public int moltiplicazione(int a, int b){
+        return a*b;
+    }
+
+    public int sottrazione (int a, int b) {return a-b;}
+
+    public double divisione(int a, int b) {
+        if (b!=0){
+            return a/b;
+        }
+        else return 0;
+    }
+}
